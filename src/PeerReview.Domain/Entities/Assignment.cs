fcf0f66436fab6735eeff0a733712ec175bd6c31
Ibrahim Aboalfoul <Ibrahim.a@isb.sa>
2025-10-31 22:23:56 +0300

    Add navigation properties to Assignment class
    Enhanced the Assignment class by adding nullable navigation
    properties for `Question` and `User`. These changes establish
    relationships with the `Question` and `User` entities, enabling
    easier navigation and data access between these entities.

--- src/PeerReview.Domain/Entities/Assignment.cs
+++ src/PeerReview.Domain/Entities/Assignment.cs
@@ -3,7 +3,9 @@ namespace PeerReview.Domain.Entities;
 public class Assignment : EntityBase
 {
     public int QuestionId { get; set; }
+    public Question? Question { get; set; }
     public int UserId { get; set; }
+    public User? User { get; set; }
     public DateTime AssignedAt { get; set; } = DateTime.UtcNow;
     public bool IsActive { get; set; } = true;
 }
